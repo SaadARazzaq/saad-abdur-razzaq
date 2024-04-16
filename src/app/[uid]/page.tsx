4d33d0202b9aa360b6ce4fefcda48bd5054a2bb2
Prime_Saad <123338307+SaadARazzaq@users.noreply.github.com>
2024-04-17 00:58:00 +0500

    Update page.tsx

--- src/app/[uid]/page.tsx
+++ src/app/[uid]/page.tsx
@@ -1,29 +1,72 @@
+// import { Metadata } from "next";
+// import { SliceZone } from "@prismicio/react";
+
+// import { createClient } from "@/prismicio";
+// import { components } from "@/slices";
+// import { notFound } from "next/navigation";
+
+// type Params = {uid:string}
+
+// export default async function Page({params}:{params:Params}) {
+//     const client = createClient()
+//     const page = await client.getByUID("page", params.uid).catch(()=>notFound())
+
+//     return <SliceZone slices={page.data.slices} components={components}/>
+// }
+
+// export async function generateMetadata({
+//     params, 
+//   } : {
+//         params: Params
+//     }):Promise<Metadata> {
+//         const client = createClient()
+//         const page = await client.getByUID("page", params.uid).catch(()=>notFound())
+
+//         return {
+//             title: page.data.meta_title,
+//             description: page.data.meta_description
+//         }
+//     }
+
 import { Metadata } from "next";
+import { notFound } from "next/navigation";
 import { SliceZone } from "@prismicio/react";
 
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
-import { notFound } from "next/navigation";
 
-type Params = {uid:string}
+type Params = { uid: string };
 
-export default async function Page({params}:{params:Params}) {
-    const client = createClient()
-    const page = await client.getByUID("page", params.uid).catch(()=>notFound())
+export default async function Page({ params }: { params: Params }) {
+  const client = createClient();
+  const page = await client
+    .getByUID("page", params.uid)
+    .catch(() => notFound());
 
-    return <SliceZone slices={page.data.slices} components={components}/>
+  return <SliceZone slices={page.data.slices} components={components} />;
 }
 
 export async function generateMetadata({
-    params, 
-  } : {
-        params: Params
-    }):Promise<Metadata> {
-        const client = createClient()
-        const page = await client.getByUID("page", params.uid).catch(()=>notFound())
+  params,
+}: {
+  params: Params;
+}): Promise<Metadata> {
+  const client = createClient();
+  const page = await client
+    .getByUID("page", params.uid)
+    .catch(() => notFound());
 
-        return {
-            title: page.data.meta_title,
-            description: page.data.meta_description
-        }
-    }
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
+}
+
+export async function generateStaticParams() {
+  const client = createClient();
+  const pages = await client.getAllByType("page");
+
+  return pages.map((page) => {
+    return { uid: page.uid };
+  });
+}
